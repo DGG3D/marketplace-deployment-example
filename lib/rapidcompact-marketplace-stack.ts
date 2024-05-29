f32b5be3318c8533f2e142edc6a9da9a2dd28c45
Niklas Appelmann <na@blackbridge.solutions>
2024-05-29 14:37:11 +0200

    Refactor stack definition and add outputs

--- lib/rapidcompact-marketplace-stack.ts
+++ lib/rapidcompact-marketplace-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { aws_s3 as s3, aws_ec2 as ec2, aws_ecs as ecs, aws_logs as logs, CfnOutput } from "aws-cdk-lib";
+import { aws_s3 as s3, aws_ec2 as ec2, aws_ecs as ecs, aws_ecr as ecr, aws_logs as logs, CfnOutput } from "aws-cdk-lib";
 import { Peer, Port } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
@@ -7,16 +7,18 @@ export class RapidcompactMarketplaceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const name = "RCMarketplace";
+    const containerImage = ecs.ContainerImage.fromRegistry(
+      "709825985650.dkr.ecr.us-east-1.amazonaws.com/darmstadt-graphics-group-gmbh/rapidcompact-renewal:0.0.4"
+    );
 
+    const name = "RCMarketplace";
     const vpc = ec2.Vpc.fromLookup(this, "VPC", {
       isDefault: true,
     });
 
-    new cdk.aws_ecs.Cluster(this, name + "Cluster", { vpc });
-
     const inputBucket = new s3.Bucket(this, "InputBucket", {});
     const outputBucket = new s3.Bucket(this, "OutputBucket", {});
+    const cluster = new ecs.Cluster(this, name + "Cluster", { vpc });
 
     const executionRole = new cdk.aws_iam.Role(this, "EcsTaskExecutionRole", {
       assumedBy: new cdk.aws_iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
@@ -52,12 +54,11 @@ export class RapidcompactMarketplaceStack extends cdk.Stack {
       retention: logs.RetentionDays.ONE_MONTH,
     });
 
-    const taskDefinition = new cdk.aws_ecs.FargateTaskDefinition(this, name + "Task", { executionRole, taskRole });
-    taskDefinition.addContainer(name + "Container", {
-      image: cdk.aws_ecs.ContainerImage.fromRegistry(
-        "709825985650.dkr.ecr.us-east-1.amazonaws.com/darmstadt-graphics-group-gmbh/rapidcompact-renewal:0.0.4"
-      ),
-      logging: cdk.aws_ecs.LogDrivers.awsLogs({
+    const taskDefinition = new ecs.FargateTaskDefinition(this, name + "Task", { executionRole, taskRole });
+    const container = new ecs.ContainerDefinition(this, name + "Container", {
+      image: containerImage,
+      taskDefinition: taskDefinition,
+      logging: ecs.LogDrivers.awsLogs({
         logGroup: logGroup,
         streamPrefix: "ecs",
       }),
@@ -68,13 +69,26 @@ export class RapidcompactMarketplaceStack extends cdk.Stack {
     // SecurityGroup and Subnet for the ECS task-run command
     const securityGroup = new cdk.aws_ec2.SecurityGroup(this, "TaskRunSG", { vpc, allowAllOutbound: true });
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(443), "Allow HTTPS access");
-    new CfnOutput(this, "SecurityGroupId", {
+    new CfnOutput(this, "SecurityGroupOutput", {
       value: securityGroup.securityGroupId,
-      description: "The ID of the security group",
     });
-    new CfnOutput(this, "PublicSubnetId", {
+    new CfnOutput(this, "SubnetOutput", {
       value: vpc.publicSubnets[0].subnetId,
-      description: "The ID of the security group",
+    });
+    new CfnOutput(this, "InputBucketOutput", {
+      value: inputBucket.bucketName,
+    });
+    new CfnOutput(this, "OutputBucketOutput", {
+      value: outputBucket.bucketName,
+    });
+    new CfnOutput(this, "ClusterOutput", {
+      value: cluster.clusterName,
+    });
+    new CfnOutput(this, "ContainerOutput", {
+      value: container.containerName,
+    });
+    new CfnOutput(this, "TaskDefinitionOutput", {
+      value: taskDefinition.family,
     });
   }
 }
